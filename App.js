19faf320ce63ba7d354a68e5bdb8fa684a71b319
Matthew Williams <143143705+MattWill89@users.noreply.github.com>
2024-01-29 22:48:48 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import React, { useState, useRef } from 'react'
+import { TodoList } from './TodoList'
+import { v4 as uuidv4 } from 'uuid'
+
+function App() {
+  const [todos, setTodos] = useState([{name: 'eat breakfast', id: 0, isComplete: false}]);
+  const addRef = useRef()
+  
+  function addTodo(event) {
+    const taskName = addRef.current.value
+    if (taskName){
+      setTodos(oldTodos => {
+        return [...oldTodos, { id: uuidv4(), name: taskName, isComplete: false}]
+      })
+      addRef.current.value = null
+    } else {
+      return null
+    }
+  }
+
+
+  return (
+    <>
+      <TodoList todos={todos} />
+      <input ref input={addRef} type="text" />
+      <button onClick={addTodo} >Add Item</button>
+      <button>Clear All</button>
+      <div>Left to do: 0</div>
+    </>
+  );
+}
+
+export default App;
